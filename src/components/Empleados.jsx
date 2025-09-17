831ef0ac17827623ed95faa1a51da2ec9d8ebd1b
LeOn-Opmac <leoncampo2707@gmail.com>
2025-09-17 08:37:40 -0500

    peticiones cambios

--- src/components/Empleados.jsx
+++ src/components/Empleados.jsx
@@ -25,7 +25,9 @@ function Empleados() {
                 apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrb2pyeWF4YnF1cXR3dnV5aGZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1MTQ0MTUsImV4cCI6MjA3MzA5MDQxNX0.nZMSWKNIve_UmSe1KEehy9ocL2FIR25QflnccDRQ998',
                 Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrb2pyeWF4YnF1cXR3dnV5aGZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1MTQ0MTUsImV4cCI6MjA3MzA5MDQxNX0.nZMSWKNIve_UmSe1KEehy9ocL2FIR25QflnccDRQ998'
             }
-        }).then(res => res.json()).then(data => setEmpleados(data));
+        }).then(()=>{
+            setEmpleados(prevState => [...prevState.filter(empleado => empleado.id !== id)]);
+        });
     }
     
     return (
